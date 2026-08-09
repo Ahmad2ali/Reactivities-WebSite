df8b48b82d04b4e558ec917aa4f31be103626b5e
Ahmad@ali <ahmada.saabneh@gmail.com>
2026-08-09 12:29:49 +0300

    End of section 9

--- client/src/Lib/api/agent.ts
+++ client/src/Lib/api/agent.ts
@@ -1,4 +1,5 @@
 import axios from "axios";
+import { store } from "../stores/store";
 
 const sleep = (delay: number) => {
     return new Promise(resolve => {
@@ -11,7 +12,13 @@ const sleep = (delay: number) => {
 const agent = axios.create({
     
     baseURL: import.meta.env.VITE_API_URL
-});
+}); 
+
+
+  agent.interceptors.request.use(config => {
+    store.uiStore.isBusy();
+    return config;
+  })
 
 agent.interceptors.response.use(async response =>{
 
@@ -22,6 +29,8 @@ agent.interceptors.response.use(async response =>{
       } catch(error) {
         console.log(error);
         return Promise.reject(error)
+    } finally {
+  store.uiStore.isIdle();
     }
 });
 
--- client/src/app/layout/NavBar.tsx
+++ client/src/app/layout/NavBar.tsx
@@ -1,46 +1,80 @@
-import { Group} from "@mui/icons-material";
-import { Box, AppBar, Toolbar, Typography, Button, Container} from "@mui/material";
+import { Group } from "@mui/icons-material";
+import {
+  Box,
+  AppBar,
+  Toolbar,
+  Typography,
+  Button,
+  Container,
+  LinearProgress,
+} from "@mui/material";
 import { NavLink } from "react-router";
 import MenuItemLink from "../shared/components/MenuItemLink";
+import { useStore } from "../../Lib/hooks/useStore";
+import { Observer } from "mobx-react-lite";
 
+export default function NavBar() {
+  const { uiStore } = useStore();
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar
+        position="static"
+        sx={{
+          backgroundImage:
+            "linear-gradient(135deg, #182a73 0%, #218aae 69%, #20a7ac 89%)",
+            position:'relative'
+        }}
+      >
+        <Container maxWidth="xl">
+          <Toolbar sx={{ display: "flex", justifyContent: "space-between" }}>
+            <Box>
+              <Box
+                component={NavLink}
+                to="/"
+                sx={{
+                  display: "flex",
+                  gap: 2,
+                  color: "white",
+                  textDecoration: "none",
+                }}
+              >
+                <Group fontSize="large" />
+                <Typography variant="h3" sx={{ fontWeight: "bold" }}>
+                  Reactivities
+                </Typography>
+              </Box>
+            </Box>
 
-export default function NavBar(){
+            <Box sx={{ display: "flex" }}>
+              <MenuItemLink to="/activities">Activities</MenuItemLink>
 
-    return (
+              <MenuItemLink to="/createActivity">Create Activity</MenuItemLink>
 
-  <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="static" sx={{
-        backgroundImage: 'linear-gradient(135deg, #182a73 0%, #218aae 69%, #20a7ac 89%)'
-        }}>
-          
-        <Container maxWidth='xl'>
-           <Toolbar sx={{display:'flex', justifyContent: 'space-between'}}>
-             <Box>
-              <Box component={NavLink} to ='/' sx={{display: "flex", gap: 2, color:"white", textDecoration: "none"}}>
-                <Group fontSize="large"/>
-                 <Typography variant="h3" sx={{fontWeight:"bold"}}>Reactivities</Typography>
-             </Box >
-      </Box>
+              <MenuItemLink to="/counter">Counter</MenuItemLink>
+            </Box>
 
-      <Box>
-        <MenuItemLink to='/activities'>
-       Activities
-       </MenuItemLink >
-
-        <MenuItemLink  to='/createActivity'
-        >Create Activity
-        </MenuItemLink >
-
-      </Box>
-
-           <Button  sx= {{color:"white", textTransform:'none'}}>
-           User menu
-           </Button>
-            </Toolbar>
+            <Button sx={{ color: "white", textTransform: "none" }}>
+              User menu
+            </Button>
+          </Toolbar>
         </Container>
-        
+        <Observer>
+          {() =>
+            uiStore.isLoading ? (
+              <LinearProgress
+                color="secondary"
+                sx={{
+                  position: "absolute",
+                  bottom: 0,
+                  left: 0,
+                  right: 0,
+                  height: 4,
+                }}
+              />
+            ) : null
+          }
+        </Observer>
       </AppBar>
-    </Box>  
-      )
-
+    </Box>
+  );
 }
